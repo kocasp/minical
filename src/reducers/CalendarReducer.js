49932dd90ac4f18e6f17494045ecd8e48ec35237
Konrad <kocasp@gmail.com>
2019-10-12 10:49:03 +0200

    describe CalendarReducer params

--- src/reducers/CalendarReducer.js
+++ src/reducers/CalendarReducer.js
@@ -15,6 +15,8 @@ const addIfExistOrCreate = (arr, val) => {
 export const CalendarReducer = (state = {}, action) => {
 
   switch (action.type) {
+    // day
+    // label
     case 'ADD_LABEL':
       if (state[action.payload.day]) {
         state[action.payload.day].labels = addIfExistOrCreate(state[action.payload.day].labels, action.payload.label)
@@ -23,11 +25,13 @@ export const CalendarReducer = (state = {}, action) => {
         state[action.payload.day] = {...state[action.payload.day], labels: [action.payload.label]};
       }
       return state;
+    // day
+    // label
     case 'REMOVE_LABEL':
-      console.log(state[action.payload.day])
-      console.log(action.payload.day)
       state[action.payload.day].labels = state[action.payload.day].labels.filter(item => item !== action.payload.label);
       return state;
+    // day
+    // task
     case 'ADD_TASK':
       if (state[action.payload.day]) {
         state[action.payload.day].tasks = addIfExistOrCreate(state[action.payload.day].tasks, action.payload.task)
@@ -35,6 +39,8 @@ export const CalendarReducer = (state = {}, action) => {
         state[action.payload.day] = {...state[action.payload.day], tasks: [action.payload.task]};
       }
       return state;
+    // day
+    // task_index
     case 'REMOVE_TASK':
       state[action.payload.day].tasks.splice(action.payload.task_index, 1)
       return state
